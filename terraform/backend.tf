3aa0240d9f202cb820f02854703879f0cee4f53e
Hardik Patel <hpatel@channelit.biz>
2022-12-15 16:52:18 -0500

    cp

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -13,3 +13,22 @@ resource "aws_s3_bucket" "cits-tfstate" {
     Environment = "Dev"
   }
 }
+resource "aws_s3_bucket_public_access_block" "block" {
+ bucket = aws_s3_bucket.cits-tfstate.id
+
+ block_public_acls       = true
+ block_public_policy     = true
+ ignore_public_acls      = true
+ restrict_public_buckets = true
+}
+resource "aws_dynamodb_table" "cits-tfstate" {
+ name           = "cits-tfstate"
+ read_capacity  = 20
+ write_capacity = 20
+ hash_key       = "LockID"
+
+ attribute {
+   name = "LockID"
+   type = "S"
+ }
+}
