b61e1f32e16146ebfa2b52a9d67a80a2d509e5c0
Hardik Patel <hpatel@channelit.biz>
2022-12-15 22:10:34 -0500

    cp

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -16,9 +16,12 @@ terraform {
   required_version = ">= 0.13"
 }
 
+# Configure the docker provider
 provider "docker" {
-#  host = "unix:///var/run/docker.sock"
 }
+
+# Create a docker image resource
+# -> docker pull nginx:latest
 resource "docker_image" "nginx" {
   name         = "nginx:latest"
   keep_locally = true
@@ -28,10 +31,34 @@ resource "docker_image" "nginx" {
 # -> same as 'docker run --name nginx -p8080:80 -d nginx:latest'
 resource "docker_container" "nginx" {
   name    = "nginx"
-  image   = docker_image.nginx.id
+  image   = docker_image.nginx.image_id
 
   ports {
     external = 8080
     internal = 80
+  }
+}
+
+# Or create a service resource
+# -> same as 'docker service create -d -p 8081:80 --name nginx-service --replicas 2 nginx:latest'
+resource "docker_service" "nginx_service" {
+  name = "nginx-service"
+  task_spec {
+    container_spec {
+      image = docker_image.nginx.repo_digest
+    }
+  }
+
+  mode {
+    replicated {
+      replicas = 2
+    }
+  }
+
+  endpoint_spec {
+    ports {
+      published_port = 8081
+      target_port    = 80
+    }
   }
 }
