54215f8c40357161018042e05295dd5f6f7256d3
Hardik Patel <hpatel@channelit.biz>
2022-12-15 20:53:08 -0500

    cp

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -17,5 +17,21 @@ terraform {
 }
 
 provider "docker" {
-  host = "unix:///var/run/docker.sock"
+#  host = "unix:///var/run/docker.sock"
+}
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = true
+}
+
+# Create a docker container resource
+# -> same as 'docker run --name nginx -p8080:80 -d nginx:latest'
+resource "docker_container" "nginx" {
+  name    = "nginx"
+  image   = docker_image.nginx.id
+
+  ports {
+    external = 8080
+    internal = 80
+  }
 }
